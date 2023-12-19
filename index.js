702320c29397e2985bd2a9f7063a4faa8f9a4ba9
Sharif-Miah <farhansharif141077@gmail.com>
2023-12-19 23:43:03 +0600

    user admin get, post delete and update code added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ejrpgrq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,10 +26,51 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviweCollection = client.db("bistroDb").collection("review");
     const cartsCollection = client.db("bistroDb").collection("carts");
 
+    // Users Collection Api
+
+    app.get('/users', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
+    app.post('/users', async(req, res)=> {
+      const user = req.body;
+      console.log(user)
+      const query = {email: user.email}
+      console.log(query)
+      const exittingUser = await usersCollection.findOne(query);
+      if(exittingUser){
+        return res.send("Already This User Used")
+      }
+      const result = await usersCollection.insertOne(user)
+
+    })
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+    app.delete('/users/admin/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // Menu collection Api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -60,6 +101,14 @@ async function run() {
       const result = await cartsCollection.insertOne(item);
       res.send(result);
     });
+
+
+    app.delete("/carts/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
